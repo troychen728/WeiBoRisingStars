792c90a0e5f51179805a0547e55520c36642312b
Xiangyu Fu <xfu18@cmc.edu>
2017-11-16 11:21:00 -0800

    ..

--- data.py
+++ data.py
@@ -3,24 +3,26 @@ import requests
 import datetime
 import time
 
-def getdata(ids, duration):
-	while 1: 
-		ts = time.time()
-		st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-		r = requests.get("https://api.weibo.com/2/users/counts.json?access_token=2.00xusAWD0YuxsZ10f9689fa7sIoHJC&uids=" + ids)
-		d = [json.loads(line) for line in r.iter_lines()]
-		data = d[0]
+def getdata(ids, timeS):
+	ts = time.time()
+	st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+	r = requests.get("https://api.weibo.com/2/users/counts.json?access_token=2.00xusAWD0YuxsZ10f9689fa7sIoHJC&uids=" + ids)
+	d = [json.loads(line) for line in r.iter_lines()]
+	data = d[0]
 
 		#print json.dumps(data)
-		f = open('data%s.json' %st,'a+')
-		for dic in data:
-			dic['timestamp'] = st 
-		f.write(json.dumps(data))
-		f.write('\n')
-		print data[45]['timestamp']
-		time.sleep(duration)
+	f = open('data%s.json' %timeS,'a+')
+	for dic in data:
+		dic['timestamp'] = st 
+	f.write(json.dumps(data))
+	f.write('\n')
+		#print data[45]['timestamp']
+	
 
 def main():
+	ts = time.time()
+	st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
 	lines = open('uid.txt','r').read().split('\n')
 	ids = lines[0]
 	for i in range(1,len(lines)):
@@ -29,7 +31,8 @@ def main():
 
 	try:
 		while True:
-			getdata(ids, 10)
+			getdata(ids, st)
+			time.sleep(600)
 
 	except KeyboardInterrupt:
 		pass
